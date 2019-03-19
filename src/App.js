e2b33fc77051f6a0d84b43f963942ce607995e41
krn-singh <karan_kalewal@yahoo.co.in>
2019-03-19 18:59:43 -0400

    Understanding state property for class based components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Karan', age: '26'},
+      { name: 'Kanwal', age: '26'}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h2>React App</h2>
-        <Person name = 'Karan' age = '26'>Hello from Karan</Person>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>Hello from Karan</Person>
         <Person name = 'Kanwal' age = '26' />
       </div>
     );

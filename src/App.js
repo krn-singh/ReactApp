a4cc2a74653c47ff05a56c3b9c488aebe18070f4
krn-singh <karan_kalewal@yahoo.co.in>
2019-03-20 10:58:44 -0400

    State change for functional components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Karan', age: '26'},
-      { name: 'Kanwal', age: '26'}
-    ]
-  }
-
-  changeNameHandler = () => {
-    this.setState({
+const App = () => {
+    const [ personState, setPersonState ] = useState({
       persons: [
-        { name: 'Karanbir', age: '26'},
-        { name: 'Kanwaljot', age: '26'}
+        { name: 'Karan', age: '26'},
+        { name: 'Kanwal', age: '26'}
       ]
-    })
-  }
+    });
 
-  render() {
+    const changeNameHandler = () => {
+      setPersonState({
+        persons: [
+          { name: 'Karanbir', age: '26'},
+          { name: 'Kanwaljot', age: '26'}
+        ]
+      })
+    }
+
     return (
       <div className="App">
         <h2>React App</h2>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>Hello from Karan</Person>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
-        <button onClick = {this.changeNameHandler}>Change Name</button>
+        <Person name = {personState.persons[0].name} age = {personState.persons[0].age}>Hello from Karan</Person>
+        <Person name = {personState.persons[1].name} age = {personState.persons[1].age} />
+        <button onClick = {changeNameHandler}>Change Name</button>
       </div>
     );
-  }
 }
 
 export default App;

197aef469d0f877b7f82759d293cb08f43633d26
krn-singh <karan_kalewal@yahoo.co.in>
2019-03-27 19:03:21 -0400

    UseRef for Functional Based Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Person.css';
 import Aux from './../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 const person = (props) => {
+    const inputElement = useRef(null);
+    
+    useEffect(() => {
+        inputElement.current.focus();
+    });
+
     return (
         <Aux>
             <p onClick={props.click}>Person name: {props.name} and age: {props.age}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change}/>
+            <input 
+            type="text" 
+            ref={inputElement}
+            onChange={props.change}
+            value ={props.name}/>
         </Aux>       
     );
 }
